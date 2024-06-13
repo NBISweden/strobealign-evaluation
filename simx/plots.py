04b43cb1bfb339d5ae1f8dcddcf79c35786aaf20
Marcel Martin <marcel.martin@scilifelab.se>
2024-06-14 00:16:42 +0200

    Simplify

--- simx/plots.py
+++ simx/plots.py
@@ -1,18 +1,11 @@
 import os,sys
 import argparse
-
 import random
 
-try:
-    import matplotlib
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-except (ImportError, RuntimeError):
-    print("COULD not import matplotlib")
-
+import matplotlib
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from matplotlib import pyplot
 
 
 def plot_accuracy(input_csv, outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=(0,500)):
@@ -46,7 +39,6 @@ def plot_accuracy(input_csv, outfolder, palette, tools, read_lengths, linewidth 
     g.tight_layout()
     # g.set(ylim=(95, 100))
     # ax.set_xticks([18,24,30,36])
-    # plt.savefig(os.path.join(outfolder, "accuracy_plot.eps"))
     plt.savefig(os.path.join(outfolder, "accuracy_plot.pdf"))
     plt.close()
 
@@ -91,46 +83,6 @@ def plot_percentage_aligned(input_csv, outfolder, palette, tools, read_lengths, 
     plt.savefig(os.path.join(outfolder, "percentage_aligned_plot.pdf"))
     plt.close()
 
-def plot_overaligned(input_csv, outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=(0,500)):
-    matplotlib.rcParams.update({'font.size': 18})
-    sns.set(font_scale=1.2)
-    # sns.set(font_scale=1.6) # for main SIM3
-    sns.set_style("whitegrid")
-
-    indata = pd.read_csv(input_csv)
-
-    # dashes = { "strobemap" : "", 
-    #             "spaced_sparse": (5,5),
-    #             "spaced_dense": (5,5),
-    #              "minstrobes2" : (1,1),
-    #              "minstrobes3" : (1,1),
-    #              "randstrobes2" : (1,1),
-    #              "randstrobes3" : (1,1),
-    #              "hybridstrobes2" : (1,1),
-    #              "hybridstrobes3" : (1,1)}
-    # print(indata)
-    g = sns.relplot(
-        data=indata, x="read_length", y="overaligned", hue="tool", style="type", linewidth = linewidth,
-        col="dataset", kind="line", hue_order = tools,  #dashes = dashes, hue="datastructure", style="datastructure",
-        col_wrap=2, col_order=["drosophila", "maize", "CHM13", "rye"], palette=palette)
-         # col_order=["SIM3"], palette=palette)
-    # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
-    # axes = g.axes
-    # g.set_titles("SIM3")
-    g.set_axis_labels("Read length", "Overaligned")
-    # g.set_xticklabels([18,24,30,36])
-    # ax.set_ylabel("% unique")
-    # ax.set_xlabel("k")
-    # axes.set_xticks([18,24,30,36] )
-    # ax.set_ylim((75, 100))
-    g.set(xlim=xlim, xticks=read_lengths) #ylim=(40, 100),
-    g.set_xticklabels(rotation=90, labels=read_lengths)
-    g.tight_layout()
-    # g.set(ylim=(95, 100))
-    # ax.set_xticks([18,24,30,36])
-    # plt.savefig(os.path.join(outfolder, "overaligned_plot.eps"))
-    plt.savefig(os.path.join(outfolder, "overaligned_plot.pdf"))
-    plt.close()
 
 
 def plot_memory_usage(input_csv, outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=(0,500)):
@@ -150,7 +102,7 @@ def plot_memory_usage(input_csv, outfolder, palette, tools, read_lengths, linewi
     # ax = sns.lineplot(data=indata, x="k", y="unique", hue="datastructure", style="chr", palette = sns.color_palette()[:7])
     # axes = g.axes
     # g.set_titles("SIM3")
-    g.set_axis_labels("Read length", "Memory usage (Gb)")
+    g.set_axis_labels("Read length", "Memory usage (GB)")
     # g.set_xticklabels([18,24,30,36])
     # ax.set_ylabel("% unique")
     # ax.set_xlabel("k")
@@ -246,21 +198,23 @@ def main(args):
 
     palette = {
     'minimap2': 'tab:blue',
-    'bwa_mem': 'tab:orange',
+    'bwamem': 'tab:orange',
     'strobealign_v071': 'tab:green',
     "strobealign_v0120_opt" : 'pink',
-    "strobealign_multicontext" : 'black'
+    "strobealign_multicontext" : 'black',
+    "strobealign-main-3a97f6b": "tab:green",
+    "strobealign-mcs-4ed851a": "black",
     }
     # read_lengths = [50, 75, 91, 100, 111, 125, 136, 150, 176, 200, 250, 300, 500]
     read_lengths = [50, 75, 100, 150, 200, 250]
     tools = ["minimap2", "bwa_mem", "strobealign_v071", "strobealign_v0120_opt", "strobealign_multicontext"]
+    tools = ["strobealign-main-3a97f6b", "strobealign-mcs-4ed851a", "bwamem"]
     xlim=(40,260)
     accuracy_csv = add_column(args.accuracy_csv)
     runtime_mem_csv = add_column(args.runtime_mem_csv)
 
     plot_accuracy(accuracy_csv, args.outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=xlim)
     plot_percentage_aligned(accuracy_csv, args.outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=xlim)
-    # plot_overaligned(accuracy_csv, args.outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=xlim)
     plot_runtime(runtime_mem_csv, args.outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=xlim)
     plot_memory_usage(runtime_mem_csv, args.outfolder, palette, tools, read_lengths, linewidth = 2.5, xlim=xlim)
 
@@ -271,11 +225,5 @@ if __name__ == '__main__':
     parser.add_argument('runtime_mem_csv', type=str, help='results file')
     parser.add_argument('outfolder', type=str,  help='outfolder to plots.')
     args = parser.parse_args()
-
-
-
-    if len(sys.argv)==1:
-        parser.print_help()
-        sys.exit()
 
     main(args)
