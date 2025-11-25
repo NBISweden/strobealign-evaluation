9a43b7bb302a51d3bf3111aa9541ea91b270441b
Marcel Martin <marcel.martin@scilifelab.se>
2025-11-25 13:13:37 +0100

    Add an end attribute to PafRecord

--- src/saeval/paf.py
+++ src/saeval/paf.py
@@ -5,6 +5,7 @@ from .get_accuracy import ReferenceInterval
 @dataclass
 class PafRecord:
     query_name: str
+    end: int  # 0: single-end, 1: paired-end R1, 2: paired-end R2
     query_length: int
     query_start: int
     query_end: int
@@ -15,13 +16,31 @@ class PafRecord:
     mapq: int
     tags: dict
 
+    def is_paired(self) -> bool:
+        return self.end != 0
+
+    def is_read1(self) -> bool:
+        return self.end == 1
+
+    def is_read2(self) -> bool:
+        return self.end == 2
+
 
 def parse_paf(file):
     for line in file:
         fields = line.split("\t")
         query_name, query_length, query_start, query_end, strand, ref_name, ref_length, ref_start, ref_end, n_matches, block_length, mapq, *tags = fields
+        end = 0
+        if query_name.endswith("/1"):
+            query_name = query_name[:-2]
+            end = 1
+        elif query_name.endswith("/2"):
+            query_name = query_name[:-2]
+            end = 2
+
         yield PafRecord(
             query_name=query_name,
+            end=end,
             query_length=int(query_length),
             query_start=int(query_start),
             query_end=int(query_end),
