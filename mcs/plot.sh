05da91993aa1b9e2ec8885ae91fc3b170d475c15
Marcel Martin <marcel.martin@scilifelab.se>
2025-12-17 14:22:27 +0100

    MCS plotting script, updated profiles and CSVs

---
+++ mcs/plot.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -euo pipefail
+
+function plot() {
+  config="$1"
+  shift
+
+  nosim04=0
+  if [[ $# -ge 1 && "$1" = "--nosim04" ]]; then
+    nosim04=1
+    shift
+  fi
+  echo "Plotting ${config} ..."
+  if [ ${nosim04} = 1 ]; then
+    actualcsv=${config}-nosim04.csv
+    # Do not show noisy profile for SIM0 and SIM4
+    grep -v 'k16s12l2u2,.*,sim[04]' ${config}.csv > ${actualcsv}
+  else
+    actualcsv=${config}.csv
+  fi
+  mkdir -p figures/${config}
+  python plots.py "$@" -c ${config}.yaml ${actualcsv} figures/${config}/
+  if [ ${nosim04} = 1 ]; then
+    rm ${config}-nosim04.csv
+  fi
+}
+
+test -e figures && rm -r figures
+
+# Figures 2 and 3
+plot chm13 --nosim04 --genome
+mv figures/chm13/genome-CHM13-{accuracy,time}.pdf figures/
+
+# X-Mapper
+plot xmapper --genome --solid
+mv figures/xmapper/genome-CHM13-accuracy.pdf figures/xmapper-CHM13-accuracy.pdf
+mv figures/xmapper/genome-CHM13-time.pdf figures/xmapper-CHM13-time.pdf
+
+# k-mer seed lookup strategy
+plot kmers --genome --solid
+mv figures/kmers/genome-CHM13-accuracy.pdf figures/kmers-CHM13-accuracy.pdf
+mv figures/kmers/genome-CHM13-time.pdf figures/kmers-CHM13-time.pdf
+
+# Long reads
+plot longreads --solid --linear-x
+mv figures/longreads/ends-se-time.pdf figures/longreads-time.pdf
+mv figures/longreads/ends-se-accuracy.pdf figures/longreads-accuracy.pdf
+
+# Full evaluation with all genomes
+plot allgenomes --nosim04
+mv figures/allgenomes/ends-se-accuracy.pdf figures/allgenomes-se-accuracy.pdf
+mv figures/allgenomes/ends-pe-accuracy.pdf figures/allgenomes-pe-accuracy.pdf
+mv figures/allgenomes/ends-se-time.pdf figures/allgenomes-se-time.pdf
+mv figures/allgenomes/ends-pe-time.pdf figures/allgenomes-pe-time.pdf
+mv figures/allgenomes/ends-se-aligned.pdf figures/allgenomes-se-aligned.pdf
+mv figures/allgenomes/ends-pe-aligned.pdf figures/allgenomes-pe-aligned.pdf
+
+plot memory
+mv figures/memory/ends-pe-memory.pdf figures/memoryusage.pdf
+
+echo "Done. Figures are in figures/"
--- mcs/run.sh
+++ mcs/run.sh
@@ -1,51 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-function run_and_plot() {
-  config="$1"
-  shift
+# Run the pipeline for each configuration (YAML file)
+# to create the .csv files
+for config in chm13 xmapper kmers longreads allgenomes memory; do
   echo "Configuration: ${config}"
   snakemake -p -c8 --configfile ${config}.yaml -- result.csv
   mv result.csv ${config}.csv
-  mkdir -p plots-${config}
-  echo Plotting ...
-  python plots.py "$@" -c ${config}.yaml ${config}.csv plots-${config}/
-}
-
-mkdir -p figures
-
-# Figures 2 and 3
-run_and_plot chm13 --legend=below --genome
-mv plots-chm13/genome-CHM13-{accuracy,time}.pdf figures/
-
-# X-Mapper
-run_and_plot xmapper --genome --solid
-mv plots-xmapper/genome-CHM13-accuracy.pdf figures/xmapper-CHM13-accuracy.pdf
-mv plots-xmapper/genome-CHM13-time.pdf figures/xmapper-CHM13-time.pdf
-
-# Seed selection strategies
-run_and_plot strategies
-run_and_plot strategies --genome --solid
-mv plots-strategies/genome-CHM13-accuracy.pdf figures/strategies-CHM13-accuracy.pdf
-mv plots-strategies/genome-CHM13-time.pdf figures/strategies-CHM13-time.pdf
-mv plots-strategies/ends-se-time.pdf figures/strategies-se-time.pdf
-mv plots-strategies/ends-pe-time.pdf figures/strategies-pe-time.pdf
-mv plots-strategies/ends-se-accuracy.pdf figures/strategies-se-accuracy.pdf
-mv plots-strategies/ends-pe-accuracy.pdf figures/strategies-pe-accuracy.pdf
-
-# Long reads
-run_and_plot longreads --solid --linear-x
-mv plots-longreads/ends-se-time.pdf figures/longreads-time.pdf
-mv plots-longreads/ends-se-accuracy.pdf figures/longreads-accuracy.pdf
-
-# Full evaluation with all genomes ("data dump")
-run_and_plot allgenomes
-mv plots-allgenomes/ends-se-accuracy.pdf figures/allgenomes-se-accuracy.pdf
-mv plots-allgenomes/ends-pe-accuracy.pdf figures/allgenomes-pe-accuracy.pdf
-mv plots-allgenomes/ends-se-time.pdf figures/allgenomes-se-time.pdf
-mv plots-allgenomes/ends-pe-time.pdf figures/allgenomes-pe-time.pdf
-mv plots-allgenomes/ends-se-aligned.pdf figures/allgenomes-se-aligned.pdf
-mv plots-allgenomes/ends-pe-aligned.pdf figures/allgenomes-pe-aligned.pdf
-
-run_and_plot memory
-mv plots-memory/ends-pe-memory.pdf figures/memoryusage.pdf
+done
