ca7cdd0028088f2ac0e96b6c0238eab9abdac3fc
Marcel Martin <marcel.martin@scilifelab.se>
2026-02-13 09:39:54 +0100

    HiFi configuration that reproduces paper plots

--- mcs/run.sh
+++ mcs/run.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Run the pipeline for each configuration (YAML file)
 # to create the .csv files
-for config in chm13 xmapper strategies longreads allgenomes memory; do
+for config in chm13 xmapper strategies longreads hifi allgenomes memory; do
   echo "Configuration: ${config}"
   snakemake -p -c8 --configfile ${config}.yaml -- result.csv
   mv result.csv ${config}.csv
